a6a9fd76980333ae880b57be14d5ef0628082cef
Charles DeLeo <flatironschool@Flatirons-MacBook-Air.local>
2019-10-08 16:00:15 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
-
+require_relative "./user.rb"
+class Student < User
+    attr_accessor :first_name, :last_name, :knowledge
+    @@all = []
+    def initialize
+        @first_name = first_name
+        @last_name = last_name
+        @knowledge = []
+        @@all << self
+    end
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
+    def knowledge
+        @knowledge
+    end
 end
